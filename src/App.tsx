381ad74bebdbd4a897960c675984bbaffbf9ca26
arnitdo <arnav.nitin.deo@gmail.com>
2024-08-28 19:41:43 +0530

    Add home page for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { GoBackN } from "./pages/go-back-n";
+import { Homepage } from "./pages/home";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={"/go-back-n"} element={<GoBackN />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route index element={<Homepage />} />
+				<Route path={"/go-back-n"} element={<GoBackN />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
